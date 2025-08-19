fd529c5a20024052c56464d546724751e9d68233
010ender <010ender@proton.me>
2025-08-18 17:21:05 -0700

    Update lib.js
    + Object is empty, object length (enumerable properties), and is a plain object with functions
    + Is a leap year, days in a month (requires year as well to account for leap years) functions

--- lib.js
+++ lib.js
@@ -1,3 +1,7 @@
+// 010ENDER'S PERSONAL JS LIBRARY
+// CREATED FOR MYSELF AND OTHERS
+// ADDS SIMPLE BUT ESSENTIAL FUNCTIONS THAT MAKE YOUR LIFE EASIER
+
 const lib = {
     // MATH
     randomNumber: function(min, max) {
@@ -43,7 +47,33 @@ const lib = {
             .toLowerCase()
             .trim() // remove whitespace before and after string
             .replace(/\s+/g, '-') // spaces to hypens
-            .replace(/[^\w-]+/g, '') // characters except for hypens go bye-bye
+            .replace(/[^\w-]+/g, '') // non-word characters except for hypens go bye-bye
             .replace(/--+/g, '-'); // a groupd of hypens shrink to a lonely one
+    },
+
+    // OBJECTS
+    objectIsEmpty: function(object) {
+        return typeof object === 'object' && object !== null && Object.keys(object).length === 0;
+    },
+
+    objectPropertiesLength: function(object) {
+        return Object.keys(object).length;
+    },
+
+    isPlainObject: function(object) {
+        return Object.prototype.toString.call(object) === '[object Object]';
+    },
+
+    // TIME
+    isLeapYear: function(year) {
+        year = Number(year);
+
+        if (year % 4 !== 0) return false;
+        if (year % 100 !== 0) return true;
+        return year % 400 === 0;
+    },
+
+    daysInMonth: function(month, year) {
+        return new Date(year, month, 0).getDate();
     }
 }
